c1c842fb0bfbfd24a47d0ebfcf0c60c6ff243da2
zuzana <zuzana.tahar@yahoo.fr>
2021-12-09 21:29:41 +0100

    connexion server mongo

--- Back/app.js
+++ Back/app.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const thing = require('./models/thing');
-
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongosh "mongodb+srv://zuzana79:seddik67@cluster0.pb8xx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -16,16 +14,7 @@ mongoose.connect('mongosh "mongodb+srv://zuzana79:seddik67@cluster0.pb8xx.mongod
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
-app.use('/api/stuff', stuffRoutes);
+
 app.use('/api/auth', userRoutes);
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Avis enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
 module.exports = app;
--- Back/server.js
+++ Back/server.js
@@ -1,8 +1,52 @@
+//importer le package HTTP pour créer un serveur
 const http = require('http');
 const app = require('./app');
 
-
-app.set('port', process.env.PORT || 3000);
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || 3000);
+//normalizePort renvoie un port valide(numéro/chaîne)
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  };
+    //si la plateforme propose un port par défaut, elle sera en écoute sur la variable process.env.PORT
+    // sinon elle prend le port 3000 par défault
+  const port = normalizePort(process.env.PORT || '3000');
+  app.set('port', port);
+  
+  //errorHandler gère les erreurs, ensuite enregistrée dans le serveur.
+  const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
+  
+  const server = http.createServer(app);
+  //écouteur d'évènements consigne le port ou le canal nommé sur lequel le serveur s'exécute dans la console.
+  server.on('error', errorHandler);
+  server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+  });
+  
+  server.listen(port);
