68381301065b01a4b9791c9a2aa6a8e09bf64f25
zuzana <zuzana.tahar@yahoo.fr>
2021-12-15 01:12:27 +0100

    angular

--- Back/app.js
+++ Back/app.js
@@ -1,6 +1,6 @@
 //app
 
-//import the Express package
+//import the express package
 const express = require('express');
 
 // Creating the API
@@ -10,6 +10,8 @@ const app = express();
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
 
+const bodyParser = require('body-parser'); 
+const path = require('path');
 //connect to MongoDB
 const mongoose = require('mongoose');
 mongoose.connect('mongosh "mongodb+srv://zuzana79:seddik67@cluster0.pb8xx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -18,11 +20,24 @@ mongoose.connect('mongosh "mongodb+srv://zuzana79:seddik67@cluster0.pb8xx.mongod
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  app.use((req, res, next) => {    //middleware to use API
+    res.setHeader('Access-Control-Allow-Origin', '*');  
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');  //Autorise special headers
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); 
+    next();
+  })
+
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
 
 app.use('/api/auth', userRoutes);
 app.use('/api/sauce', sauceRoutes);
+
+app.use('/images', express.static(path.join(__dirname, 'images'))); 
+app.use(bodyParser.json());
+
+
+ 
 
 module.exports = app;
